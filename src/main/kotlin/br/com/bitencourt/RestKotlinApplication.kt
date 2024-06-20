e9971240a96669a7b747dfe5c1120fb5aa19f096
PedroBitencourt <pedro.bitencourt42@gmail.com>
2024-06-20 11:00:37 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/bitencourt/RestKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.bitencourt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/bitencourt/RestKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.bitencourt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
